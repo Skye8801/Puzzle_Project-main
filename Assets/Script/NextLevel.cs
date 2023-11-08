174aa7ef27fcf6e67bcbb3acc9ce8a074dfbd713
Skye8801 <fridagottwald@gmail.com>
2023-11-08 11:02:10 +0100

    Puzzle

---
+++ Assets/Script/NextLevel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : MonoBehaviour
+{
+    public void PuzzleLvl2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
--- Assets/Script/Zoom.cs
+++ Assets/Script/Zoom.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Zoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float zoom;
+    private float zoomMultiplier = 4f;
+    private float minZoom = 0.2f;
+    private float maxZoom = 15f;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+
+    [SerializeField] private Camera cam;
+    private void Start()
     {
-        
+        zoom = cam.orthographicSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom -= scroll * zoomMultiplier;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
     }
 }
