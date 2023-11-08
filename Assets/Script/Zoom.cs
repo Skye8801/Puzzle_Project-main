0833f9fb471969241fd6f97cd75c4f62afd6468f
Skye8801 <fridagottwald@gmail.com>
2023-11-08 11:11:53 +0100

    Puzzle

--- Assets/Script/Zoom.cs
+++ Assets/Script/Zoom.cs
@@ -1,30 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
+
 
 public class Zoom : MonoBehaviour
 {
-    private float zoom;
-    private float zoomMultiplier = 4f;
-    private float minZoom = 0.2f;
-    private float maxZoom = 15f;
-    private float velocity = 0f;
-    private float smoothTime = 0.25f;
+ 
 
     [SerializeField] private Camera cam;
-    private void Start()
-    {
-        zoom = cam.orthographicSize;
-    }
 
-    private void Update()
+    float zoom = 10f;
+    
+
+    void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        zoom -= scroll * zoomMultiplier;
-        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+        if (cam.orthographic)
+        {
+            cam.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom;
+        }
+        else
+        {
+            cam.fieldOfView += Input.GetAxis("Mouse ScrollWheel") * zoom;
+        }
     }
 }
